3a76da3c2bd33d3c95efb53202eeddab23677ac9
Ken Takahash <takaken@redimpulz.com>
2017-03-25 04:49:43 +0900

    起床と就寝の登録ができるように

--- app/src/main/java/com/example/yugo/mysecretary/SecondSet.java
+++ app/src/main/java/com/example/yugo/mysecretary/SecondSet.java
@@ -3,19 +3,26 @@ package com.example.yugo.mysecretary;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.app.TimePickerDialog;
+import android.widget.Spinner;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
-import static com.example.yugo.mysecretary.R.id.textView;
+// import static com.example.yugo.mysecretary.R.id.textView;
 
-public class SecondSet extends AppCompatActivity {
+import android.widget.TimePicker;
+
+public class SecondSet extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
     RealmResults<SecondTimepointsDB> secondTimepointsDBs = null;
     RealmResults<SecondTimeintervalsDB> secondTimeintervalsDBs = null;
     RealmResults<SleeptimeDB> sleeptimeDBs = null;
@@ -24,54 +31,103 @@ public class SecondSet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondset);
+
         secondTimepointsDBs = getSecondTimepointsDBs();
         secondTimeintervalsDBs = getSecondTimeintervalsDBs();
         sleeptimeDBs = getSleeptimeDBs();
 
         //起床・就寝時間の表示
-        if(sleeptimeDBs != null && sleeptimeDBs.size() != 0) {
+        if(sleeptimeDBs != null && sleeptimeDBs.size() >= 2) {
             SleeptimeDB tmp0 = sleeptimeDBs.get(0);
             SleeptimeDB tmp1 = sleeptimeDBs.get(1);
             if (tmp0.getTime() != null) {
                 Date jikan = tmp0.getTime();
-                SimpleDateFormat sdf = new SimpleDateFormat("mm");
-                String s = sdf.format(jikan);
-                TextView textView = (TextView) findViewById(R.id.textView17);
-                textView.setText(s + "分");
+                setSleeptimeTextView(0, jikan);
             } else {
                 TextView textView = (TextView) findViewById(R.id.textView17);
-                textView.setText(00 + "分");
+                textView.setText("未設定");
             }
             if (tmp1.getTime() != null) {
                 Date jikan = tmp1.getTime();
-                SimpleDateFormat sdf = new SimpleDateFormat("mm");
-                String s = sdf.format(jikan);
-                TextView textView = (TextView) findViewById(R.id.textView19);
-                textView.setText(s + "分");
+                setSleeptimeTextView(1, jikan);
             } else {
                 TextView textView = (TextView) findViewById(R.id.textView19);
-                textView.setText(00 + "分");
+                textView.setText("未設定");
             }
         }else{
             TextView textView0 = (TextView) findViewById(R.id.textView17);
             TextView textView1 = (TextView) findViewById(R.id.textView19);
-
-            textView0.setText(00 + "分");
-            textView1.setText(00 + "分");
-
+            textView0.setText("未設定");
+            textView1.setText("未設定");
         }
-
-
 
     }
 
-    private RealmResults<SecondTimepointsDB> getSecondTimepointsDBs(){
+    public void setSleeptimeTextView(int idx, Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
+        String s = sdf.format(date);
+        if (idx==0) {
+            TextView textView = (TextView) findViewById(R.id.textView17);
+            textView.setText(s);
+        }
+        if (idx==1) {
+            TextView textView = (TextView) findViewById(R.id.textView19);
+            textView.setText(s);
+        }
+    }
 
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Spinner spinner = (Spinner)findViewById(R.id.spinner3);
+        TextView textView = (TextView)findViewById(R.id.textView4);
+
+        int idx = spinner.getSelectedItemPosition();
+        SleeptimeDB sleeptimeDB = new SleeptimeDB();
+
+        Calendar sleeptime = Calendar.getInstance();
+        sleeptime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        sleeptime.set(Calendar.MINUTE, minute);
+        sleeptimeDB.setId(idx);
+        sleeptimeDB.setTime(sleeptime.getTime());
+
+        try {
+            sleeptimeDBs.get(idx);
+            updateSleepTime(sleeptimeDB);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            createSleepTime(sleeptimeDB);
+        }
+
+        setSleeptimeTextView(idx, sleeptime.getTime());
+    }
+
+    private void updateSleepTime(SleeptimeDB db) {
+        long idx = db.getId();
+        Date date = db.getTime();
+        Realm realm = Realm.getInstance(this);
+        SleeptimeDB tmp = sleeptimeDBs.get((int)idx);
+        realm.beginTransaction();
+        tmp.setTime(date);
+        realm.commitTransaction();
+    }
+
+    private void createSleepTime(SleeptimeDB db) {
+        SleeptimeDB model = new SleeptimeDB();
+        Realm realm = Realm.getInstance(this);
+        realm.beginTransaction();
+        realm.copyToRealm(db);
+        realm.commitTransaction();
+    }
+
+    public void showTimePickerDialog(View v) {
+        AppCompatDialogFragment newFragment = new TimePick();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    private RealmResults<SecondTimepointsDB> getSecondTimepointsDBs(){
         Realm realm = Realm.getInstance(this);
         RealmQuery<SecondTimepointsDB> query = realm.where(SecondTimepointsDB.class);
-
         return query.findAll();
-
     }
 
     private RealmResults<SecondTimeintervalsDB> getSecondTimeintervalsDBs(){
@@ -93,7 +149,6 @@ public class SecondSet extends AppCompatActivity {
         //アクティビティを起動
         startActivity(ListView);
     }
-
 
 }
 
